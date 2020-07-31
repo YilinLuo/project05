28ceae7271997aa40d59bec82b2d4fc3e61d2167
Alejandro Ramirez <aramir10@u.rochester.edu>
2020-07-30 23:41:55 -0400

    updated project number

--- inc/home-html-top.php
+++ inc/home-html-top.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>CSC 174 - Project3</title>
+	<title>CSC 174 - Project4</title>
 	<link rel="stylesheet" type="text/css" href="css/home.css">
 
 	<meta name="viewport" content="width=device-width, initial-scale=1"></head><body>
