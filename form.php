36c13def090c36177217e3f6c95f0177bc8bd96b
Iris Qi Miao <irism824-NF@users.noreply.github.com>
2020-08-01 00:53:21 +0800

    add container

--- form.php
+++ form.php
@@ -8,6 +8,8 @@
 </head>
 <body>
 
+	<div class="container">
+
 	<h1>New/Edit(dynamic) Student Information</h1>
 	<!--use php to create dynamic h1, or create 2 forms(new info and edit info). Don't know which way would be implmented -->
 
@@ -52,7 +54,9 @@
 
 	</form>
 
-	
+</div>
+
+
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
