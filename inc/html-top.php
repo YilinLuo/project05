e221dd47a5fc451ae872b718ef0a929a87ed2be6
Alejandro Ramirez <aramir10@u.rochester.edu>
2020-07-30 23:43:00 -0400

    Update html-top.php

--- inc/html-top.php
+++ inc/html-top.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="description" content="Project 3 London Team">
-		<title>Project 3</title>
+		<title>Project 4</title>
 		<link rel="stylesheet" href="css/styles.css">
 		<link rel="stylesheet" href="css/navigation.css">
 		<link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300&family=Niconne&display=swap" rel="stylesheet">
