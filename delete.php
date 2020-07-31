2e0d8cf022e48cb495bdff47aacb57ee549641d1
Alejandro Ramirez <aramir10@u.rochester.edu>
2020-07-30 23:07:32 -0400

    updated delete.php with current database

--- delete.php
+++ delete.php
@@ -8,7 +8,7 @@ if (isset($_GET['id']) && is_numeric($_GET['id'])) {
 	$id = $_GET['id'];
 
 	// delete the entry
-	$result = mysqli_query($connection, "DELETE FROM aramirez_phonelist WHERE id=$id");
+	$result = mysqli_query($connection, "DELETE FROM students WHERE id=$id");
 
 	// redirect back to the homepage to see the results
 	header("Location: index.php");
