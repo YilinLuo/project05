04dbe5105a6330cf4aee5f22f732a2c2aa774fb3
bowu9458 <67699284+bowu9458@users.noreply.github.com>
2020-08-02 00:08:30 -0400

    login to access "modify content"

--- secondary.php
+++ secondary.php
@@ -1,6 +1,9 @@
 <?php include "inc/html-top.php"; ?>
 
-<a href="secondary-modify.php" class="modify-button" title="I view this as a developer tool. So I decide to put this button on the right side without destroying the look of centered actual centent">Modify Content</a>
+<?php if(isset($_SESSION['username'])) { ?>
+	<!-- login to access the modify content -->
+    <a href="secondary-modify.php" class="modify-button" title="I view this as a developer tool. So I decide to put this button on the right side without destroying the look of centered actual centent">Modify Content</a>
+<?php } ?>
 
 <article>
 	<header class="blacktop2">
