ef79c3a6d3d82a6e4c1c22ffb6e7acf40455308a
efreling <67718969+efreling@users.noreply.github.com>
2020-08-08 16:09:19 -0400

    unicode on register.php and login.php

--- register.php
+++ register.php
@@ -132,8 +132,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
                         <div class="form-group">
                             <input type="submit" class="btn submit_button float left" value="Submit">
-                            <a class = "btn cancel_button login_cancel" href="index.php">Home</a>
-                            <a class = "btn cancel_button login_cancel" href="studentlist.php">Student List</a> 
+                            <a class = "btn cancel_button login_cancel" href="index.php">&#10006; Home</a>
+                            <a class = "btn cancel_button login_cancel" href="studentlist.php">&#10006; Student List</a> 
                         </div>
 
                          <br><br>
